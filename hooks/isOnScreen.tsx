3f274197c6b4214b19d2d6cc678179d28b7481f4
Anthony Ameh <antonineutron@gmail.com>
2025-08-08 10:45:33 +0100

    updated dependency

--- hooks/isOnScreen.tsx
+++ hooks/isOnScreen.tsx
@@ -1,23 +1,25 @@
 "use client"
 import { RefObject, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react"
 
-export default function isOnScreen(ref: RefObject<HTMLElement>) {
+export default function isOnScreen(ref: RefObject<HTMLElement | null>) {
 
   const [isIntersecting, setIntersecting] = useState(false)
   
 
-  const observer = useRef<IntersectionObserver>()
+  const observer = useRef<IntersectionObserver | null>(null)
   // useMemo(() => , [ref])
 
 
   useLayoutEffect(() => {
+    if (!ref.current) return
+    
     if(observer.current == null){
       observer.current = new IntersectionObserver(
         ([entry]) => setIntersecting(entry.isIntersecting)
       )
     }
     observer.current.observe(ref.current as Element)
-    return () => observer.current!!.disconnect()
+    return () => observer.current?.disconnect()
   }, [])
 
   return isIntersecting
