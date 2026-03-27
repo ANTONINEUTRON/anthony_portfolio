59e0a422c6bb4b7ea729c74a1628b02fb87c0c48
Anthony Ameh <antonineutron@gmail.com>
2026-03-27 01:22:05 +0100

    updated engine

--- components/SideSection.tsx
+++ components/SideSection.tsx
@@ -56,15 +56,15 @@ const VertTabs = ()=>{
                 </Link>
             </li>
             <li>
-                <Link href={{ pathname: "/", hash: "projects"}} className={`${viContext!!.stringValue.includes("projects") && "bg-primary"} text-slate-900 inline-flex items-center hover:bg-primary px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 w-full`}>
-                    <Code className="w-4 h-4 me-2 "/> 
-                    Projects
+                <Link href={{ pathname: "/", hash: "experience" }} className={`${viContext!!.stringValue.includes("experience") && "bg-primary"} text-slate-900 inline-flex items-center hover:bg-primary active:bg-primary px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 w-full`}>
+                    <svg className="w-4 h-4 me-2 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18"><path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" /></svg>
+                    Experience
                 </Link>
             </li>
             <li>
-                <Link href={{ pathname: "/", hash: "experience"}} className={`${viContext!!.stringValue.includes("experience") && "bg-primary"} text-slate-900 inline-flex items-center hover:bg-primary active:bg-primary px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 w-full`}>
-                    <svg className="w-4 h-4 me-2 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18"><path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/></svg>
-                    Experience
+                <Link href={{ pathname: "/", hash: "projects"}} className={`${viContext!!.stringValue.includes("projects") && "bg-primary"} text-slate-900 inline-flex items-center hover:bg-primary px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 w-full`}>
+                    <Code className="w-4 h-4 me-2 "/> 
+                    Projects
                 </Link>
             </li>
         </ul>
